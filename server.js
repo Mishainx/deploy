29b5765f1a04e53ad72ae88233a8a237784bb68f
mishainx <fer.r@live.com.ar>
2023-05-17 07:57:28 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+export const app = express();
+
+//Configuración del servidor
+const httpServer = app.listen(8080,'0.0.0.0', async () => {
+    console.log(`Server running on port 8080`);
+});
+
+app.get("/", (req,res)=>{
+    res.send("probando")
+})
