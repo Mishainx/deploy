79034130bf61fac6505cc51b5097b7656223f1c4
mishainx <fer.r@live.com.ar>
2023-05-17 08:01:59 -0300

    modificaciones

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import  express  from "express";
 export const app = express();
 
 //Configuración del servidor
-const httpServer = app.listen(8080,'0.0.0.0', async () => {
+const httpServer = app.listen(8080,'154.49.246.83', async () => {
     console.log(`Server running on port 8080`);
 });
 
